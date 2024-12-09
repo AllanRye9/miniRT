b8055c7c5b1c53e13e1fa7e18a828ea83e11a935
surajbartoula <suraj_bartoula@yahoo.com>
2024-12-09 12:54:05 +0400

    working on thread func

--- src/renderer/render_scene.c
+++ src/renderer/render_scene.c
@@ -6,11 +6,39 @@
 /*   By: sbartoul <sbartoul@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/07 19:02:22 by sbartoul          #+#    #+#             */
-/*   Updated: 2024/12/08 18:32:27 by sbartoul         ###   ########.fr       */
+/*   Updated: 2024/12/09 12:53:03 by sbartoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
+
+void	scale_adjacent(t_thread_data *tdata)
+{
+	int	i_w;
+	int	j_h;
+	int	h;
+	int	w;
+
+	j_h = tdata->y_scale_start - 1;
+	while (++j_h < tdata->y_scale_end)
+	{
+		i_w = -1;
+		while (++i_w < tdata->scene->settings.disp_w)
+		{
+			w = round((i_w / (double)tdata->scene->settings.disp_w)
+					* tdata->width);
+			h = round((j_h / (double)tdata->scene->settings.disp_h)
+					* tdata->height);
+			w = min(w, tdata->width - 1);
+			h = min(h, tdata->height - 1);
+			*(unsigned int *)(tdata->scene->disp->display_addr
+					+ (j_h * tdata->scene->settings.disp_w + i_w)
+					* tdata->scene->disp->bpp) = *(unsigned int *)(tdata->addr
+					+ (h * tdata->width + w) * tdata->scene->disp->bpp);
+		}
+	}
+	return (NULL);
+}
 
 void	renderwith_threads(t_thread_data *tdata)
 {
