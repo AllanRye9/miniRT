b7f7b2c12a7be1742f3d2e5b689ee8b146960b99
Oryema Allan <34004636+AllanRye9@users.noreply.github.com>
2024-12-10 02:04:48 +0000

    shading files modified

--- src/shading/utils3.c
+++ src/shading/utils3.c
@@ -1,20 +1,25 @@
 #include "miniRT.h"
 
-double  is_shadow(t_scene *scene, int x, t_vector *p, \
-        double *p)
-
+double	vec_magnitude(const t_vector *vec)
 {
-    double			distance;
+	return (sqrt(vec->x * vec->x + vec->y * vec->y \
+			+ vec->z * vec->z));
+}
+
+bool	is_shadow(t_scene *scene, int light_idx, t_vector *itx_point,
+			double *angle)
+{
+	double			distance;
 	int				i;
 	t_ray			ray;
 	t_intersections	arr;
 	t_intersection	*itx;
 
-	sub_vec(&ray.dir, &scene->lights[i].position, i);
+	sub_vec(&ray.dir, &scene->lights[light_idx].position, itx_point);
 	distance = vec_magnitude(&ray.dir);
 	scale_vec(&ray.dir, &ray.dir, 1 / distance);
-	ray.origin = *i;
-	if (check_spotlight(scene, i, &ray, angle) == true)
+	ray.origin = *itx_point;
+	if (check_spotlight(scene, light_idx, &ray, angle) == true)
 		return (true);
 	i = -1;
 	arr.count = 0;
