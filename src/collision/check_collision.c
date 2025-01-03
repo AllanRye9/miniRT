4e79ad15c30a7f0d4b3405402270243bf8f4fa17
surajbartoula <suraj_bartoula@yahoo.com>
2025-01-03 12:23:25 +0400

    did_collide instead of collided

--- src/collision/check_collision.c
+++ src/collision/check_collision.c
@@ -6,7 +6,7 @@
 /*   By: sbartoul <sbartoul@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/30 17:28:10 by sbartoul          #+#    #+#             */
-/*   Updated: 2025/01/03 01:39:36 by sbartoul         ###   ########.fr       */
+/*   Updated: 2025/01/03 12:21:21 by sbartoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,76 +15,76 @@
 static bool	handle_complex_collision(t_shape *shape1, t_shape *shape2,
 	bool resolve, t_shape *t_s)
 {
-	bool	collided;
+	bool	did_collide;
 
-	collided = false;
+	did_collide = false;
 	if (shape1->type == SPHERE && shape2->type == CUBE && t_s != shape2)
-		collided = sphere_box_collision(shape1, shape2, false, resolve);
+		did_collide = sphere_box_collision(shape1, shape2, false, resolve);
 	else if (shape1->type == CUBE && shape2->type == SPHERE && t_s != shape2)
-		collided = sphere_box_collision(shape2, shape1, true, resolve);
+		did_collide = sphere_box_collision(shape2, shape1, true, resolve);
 	else if (shape1->type == CUBE && shape2->type == PLANE)
-		collided = box_plane_collision(shape1, shape2, resolve);
+		did_collide = box_plane_collision(shape1, shape2, resolve);
 	else if (shape1->type == CYLINDER && shape2->type == SPHERE
 		&& t_s != shape2)
-		collided = cylinder_sphere_collision(shape1, shape2, resolve);
+		did_collide = cylinder_sphere_collision(shape1, shape2, resolve);
 	else if (shape1->type == SPHERE && shape2->type == CYLINDER
 		&& t_s != shape2)
-		collided = cylinder_sphere_collision(shape2, shape1, resolve);
+		did_collide = cylinder_sphere_collision(shape2, shape1, resolve);
 	else if (shape1->type == CUBE && shape2->type == CYLINDER && t_s != shape2)
-		collided = box_cylinder_collision(shape1, shape2);
+		did_collide = box_cylinder_collision(shape1, shape2);
 	else if (shape1->type == CYLINDER && shape2->type == CUBE && t_s != shape2)
-		collided = box_cylinder_collision(shape2, shape1);
-	return (collided);
+		did_collide = box_cylinder_collision(shape2, shape1);
+	return (did_collide);
 }
 
 bool	same_object_collision(t_shape *shape1, t_shape *shape2, bool resolve,
 	t_shape *t_s)
 {
-	bool	collided;
+	bool	did_collide;
 
-	collided = false;
+	did_collide = false;
 	if (shape1->type == SPHERE && shape2->type == SPHERE)
 	{
-		collided = sphere_sphere_collision(shape1, shape2);
-		if (collided && resolve)
+		did_collide = sphere_sphere_collision(shape1, shape2);
+		if (did_collide && resolve)
 			resolve_sphere_sphere(shape1, shape2, t_s);
 	}
 	else if (shape1->type == CUBE && shape2->type == CUBE)
 	{
-		collided = box_box_collision(shape1, shape2, false);
-		if (collided && resolve)
+		did_collide = box_box_collision(shape1, shape2, false);
+		if (did_collide && resolve)
 			box_box_collision_resolution(shape1, shape2, t_s);
 	}
 	else if (shape1->type == CYLINDER && shape2->type == CYLINDER)
-		collided = cylinder_cylinder_collision(shape1, shape2);
+		did_collide = cylinder_cylinder_collision(shape1, shape2);
 	else if (shape1->type == CONE && shape2->type == CONE)
-		collided = cone_cone_collision(shape1, shape2);
-	return (collided);
+		did_collide = cone_cone_collision(shape1, shape2);
+	return (did_collide);
 }
 
 bool	check_collision(t_shape *shape1, t_shape *shape2, bool resolve,
 	t_shape *transformed_shape)
 {
-	bool	collided;
+	bool	did_collide;
 
-	collided = false;
+	did_collide = false;
 	if (shape1->type == shape2->type)
-		collided = same_object_collision(shape1, shape2, resolve,
+		did_collide = same_object_collision(shape1, shape2, resolve,
 				transformed_shape);
 	else if (shape1->type == SPHERE && shape2->type == PLANE)
 	{
-		collided = sphere_plane_collision(shape1, shape2);
-		if (collided && resolve)
+		did_collide = sphere_plane_collision(shape1, shape2);
+		if (did_collide && resolve)
 			sphere_plane_collision_resolution(shape1, shape2);
 	}
 	else if (shape1->type == CYLINDER && shape2->type == PLANE)
 	{
-		collided = cylinder_plane_collision(shape1, shape2);
-		if (collided && resolve)
+		did_collide = cylinder_plane_collision(shape1, shape2);
+		if (did_collide && resolve)
 			cylinder_plane_collision_resolution(shape1, shape2);
 	}
 	else
-		collided = handle_complex_collision(shape1, shape2, resolve,
+		did_collide = handle_complex_collision(shape1, shape2, resolve,
 				transformed_shape);
-	return (collided);
+	return (did_collide);
 }
